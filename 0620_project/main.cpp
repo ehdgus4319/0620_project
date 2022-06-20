b4e11b5415235080325c8bf47a9d928e90606560
kdh <ehdgus0512@gmail.com>
2022-06-20 20:33:40 +0900

    Initial Commit

---
+++ 0620_project/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  0620_project
+//
+//  Created by 김동현 on 2022/06/20.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}

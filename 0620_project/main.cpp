de400ba791532bb25a65f06419bb04d064a452f6
kdh <ehdgus0512@gmail.com>
2022-06-22 00:13:48 +0900

    동적 바인딩

--- 0620_project/main.cpp
+++ 0620_project/main.cpp
@@ -9,29 +9,79 @@
 #include <cstdio>
 using namespace std;
 
-class Base{
+class Weapon{
 public:
-    void Print() {
-        cout << "From Base!!!" << endl;
+    Weapon(int power) : power(power)
+    {
+        cout << "Weapon(int)" << endl;
+    }
+    
+    virtual void Use()
+    {
+        cout << "Weapon::Use()" << endl;
+    }
+protected:
+    int power;
+};
+
+class Sword : public Weapon
+{
+public:
+    Sword(int power) : Weapon(power)
+    {
+        cout << "Sword(int)" << endl;
+    }
+    
+    void Use()
+    {
+        cout << "Sword::Use()" << endl;
+        Swing();
+    }
+private:
+    void Swing()
+    {
+        cout << "Swing sword" << endl;
     }
 };
 
-class Derived : public Base {
+class Magic : public Weapon
+{
 public:
-    void Print(){
-        cout << "From Derived!!!" << endl;
+    Magic(int power, int ManaCost) : Weapon(power), manaCost(manaCost)
+    {
+        cout << "Magic(int, int)" << endl;
     }
-    void Print2(){
-        cout << "Derived2" << endl;
+    void Use()
+    {
+        cout << "Magic::Use" << endl;
+        Cast();
     }
+private:
+    void Cast()
+    {
+        cout << "Cast magic" << endl;
+    }
+    int manaCost;
 };
+
 
 int main() {
+    Sword mySword(10);
+    Magic myMagic(15, 7);
     
-    Base *b = new Derived();
-    b->Print();
+    mySword.Use();
+    myMagic.Use();
+    cout << endl << endl;
     
-    delete b;
+    Weapon *currentWeapon;
+    currentWeapon = &mySword;
+    
+    currentWeapon->Use();
+    
+    currentWeapon = &myMagic;
+    currentWeapon->Use();
+    
+   
     
     return 0;
 }
